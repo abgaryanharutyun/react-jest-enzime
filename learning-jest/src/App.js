0af86eea1b6969e937ae31108a6dfa3ef9bed1f9
Harut Abgaryan <harutyun.abgaryan@gmail.com>
2019-04-14 19:14:59 +0400

    2

--- learning-jest/src/App.js
+++ learning-jest/src/App.js
@@ -5,8 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> Hello World </h1>
+      <div className="App" data-test='component-app'>
+        <h1 data-test={'counter-display'}> The counter  </h1>
+        <button data-test={'increment-button'} > Increment </button> 
       </div>
     );
   }
--- learning-jest/src/App.test.js
+++ learning-jest/src/App.test.js
@@ -6,5 +6,26 @@ import App from './App';
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 test('renders without crashing', () => {
   const wrapper = shallow(<App />);
-  expect(wrapper).toBeTruthy();
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+test('test increment button', ()=> {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
+});
+
+test('render count display', ()=> {
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
+});
+
+test('count started at 0', () => {
+
+});
+
+test('click button increment count display', () => {
+
+})
